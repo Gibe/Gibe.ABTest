5b2b3419d2a93337bf5475784d084e803e17866d
Steve Temple <steve@gibedigital.com>
2016-08-02 18:25:28 +0100

    Nuspec file & SQL install script

---
+++ Gibe.AbTest/SQL/install.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE AbExperiment (
+	[Key] VARCHAR(50) NOT NULL PRIMARY KEY,
+	[StartDate] DATETIME NOT NULL,
+	[EndDate] DATETIME NULL,
+	[Enabled] BIT NOT NULL DEFAULT(1),
+	[Weight] INT NOT NULL
+)
+GO
+
+CREATE TABLE AbVariation (
+	[Id] INT NOT NULL PRIMARY KEY,
+	[ExperimentKey] VARCHAR(50) NOT NULL,
+	[VariationNumber] INT NOT NULL,
+	[Enabled] BIT NOT NULL DEFAULT(1),
+	[Weight] INT NOT NULL,
+	[Definition] NVARCHAR(MAX)
+)
+GO
+
+ALTER TABLE AbVariation
+	ADD CONSTRAINT [FK_AbVariation_Experiment]
+	FOREIGN KEY ([ExperimentKey])
+	REFERENCES [AbExperiment] ([Key])
+GO
